46e6efdbff9a5c532a9a768ad1886c7fc2a53ab6
Alejandra Miranda Reyes <ale.miranda890@gmail.com>
2022-01-20 17:12:18 -0500

    converting the app code to classes

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,13 +1,13 @@
-const Hangman = function(word, numbGuessing) {
-    this.word= word.toLowerCase().split('')
-    this.numbGuessing= numbGuessing
-    this.guessedLetters= []
-    this.status = 'playing'
+class Hangman {
+    constructor(word, numbGuessing) {
+        this.word= word.toLowerCase().split('')
+        this.numbGuessing= numbGuessing
+        this.guessedLetters= []
+        this.status = 'playing'
+    }
 
-}
-
-Hangman.prototype.puzzleWord= function(){
-    let puzzle = ''
+    puzzleWord() {
+        let puzzle = ''
 
     this.word.forEach((letter) => {
         if(this.guessedLetters.includes(letter) || letter === ' ') {
@@ -15,12 +15,12 @@ Hangman.prototype.puzzleWord= function(){
         } else {
             puzzle += '*'
         }
-    });
-    return puzzle
-}
+        });
+        return puzzle
+    }
 
-Hangman.prototype.makeGuess= function(guess) {
-    guess = guess.toLowerCase()
+    makeGuess(guess) {
+        guess = guess.toLowerCase()
     const isUnique = !this.guessedLetters.includes(guess)
     const isBadGuess = !this.word.includes(guess)
 
@@ -34,33 +34,29 @@ Hangman.prototype.makeGuess= function(guess) {
     if (isUnique && isBadGuess) {
         this.numbGuessing--
     }
-
     this.calculateStatus()
-}
-
-
-Hangman.prototype.calculateStatus = function() {
-    const finished = this.word.every((letter) => {
-        return this.guessedLetters.includes(letter)
-    })
-
-
-    if (this.numbGuessing === 0) {
-        this.status = 'failed'
-    } else if (finished) {
-        this.status = 'finished'
-    } else {
-        this.status = 'playing'
     }
-}
 
-
-Hangman.prototype.getStatusMessage = function() {
-    if (this.status === 'playing') {
-        return `Guesses left: ${this.numbGuessing}`
-    } else if (this.status === 'failed') {
-        return `Nice try! The word was: ${this.word.join('')}`
-    } else {
-        return `Great work! You guessed the word`
+    calculateStatus(){
+        const finished = this.word.every((letter) => {
+            return this.guessedLetters.includes(letter)
+        })
+        if (this.numbGuessing === 0) {
+            this.status = 'failed'
+        } else if (finished) {
+            this.status = 'finished'
+        } else {
+            this.status = 'playing'
+        }
+    }
+    
+    getStatusMessage() {
+        if (this.status === 'playing') {
+            return `Guesses left: ${this.numbGuessing}`
+        } else if (this.status === 'failed') {
+            return `Nice try! The word was: ${this.word.join('')}`
+        } else {
+            return `Great work! You guessed the word`
+        }
     }
 }
