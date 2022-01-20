7e083a0d3d94617593314d59145de50a5fa59116
Alejandra Miranda Reyes <ale.miranda890@gmail.com>
2022-01-20 15:09:27 -0500

    adding methode to give users the ability to make a guess

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,7 +1,7 @@
 const Hangman = function(word, numbGuessing) {
     this.word= word.toLowerCase().split('')
     this.numbGuessing= numbGuessing
-    this.guessedLetters= ['c', 'e']
+    this.guessedLetters= []
 }
 
 Hangman.prototype.puzzleWord= function(){
@@ -16,14 +16,33 @@ Hangman.prototype.puzzleWord= function(){
     });
     return puzzle
 }
+
+Hangman.prototype.makeGuess= function(guess) {
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.word.includes(guess)
+
+    if (isUnique) {
+        this.guessedLetters.push(guess)
+    }
+    if (isUnique && isBadGuess) {
+        this.numbGuessing--
+    }
+}
  
 
-const game1= new Hangman('cOLA', 3,1)
-const game2= new Hangman('bye', 2, 0)
-
-
+const game1= new Hangman('cOLA', 3)
+game1.makeGuess('o')
+game1.makeGuess('p')
+game1.makeGuess('t')
 console.log(game1.puzzleWord())
+console.log(game1.numbGuessing)
+
+
+const game2= new Hangman('bye', 2)
+game2.makeGuess('y')
 console.log(game2.puzzleWord())
+console.log(game2.numbGuessing)
 
 
 
